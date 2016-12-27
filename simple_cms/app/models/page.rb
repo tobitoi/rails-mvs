801203ded6e5d978f5656071396f7923bc950f02
andifadli <andi.fadli@jurnal.id>
2016-12-27 23:09:33 +0700

    no message

---
+++ simple_cms/app/models/page.rb
@@ -0,0 +1,45 @@
+class Page < ActiveRecord::Base
+
+  belongs_to :subject
+  has_many :sections
+  has_and_belongs_to_many :editors, :class_name => "AdminUser"
+
+  acts_as_list :scope => :subject
+
+  before_validation :add_default_permalink
+  after_save :touch_subject
+
+  validates_presence_of :name
+  validates_length_of :name, :maximum => 255
+  validates_presence_of :permalink
+  validates_length_of :permalink, :within => 3..255
+  #use presence_of witih length_of to disallow space
+  validates_uniqueness_of :permalink
+  #for unique value by subject use ":scope => :subject_id"
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("pages.position ASC") }
+  scope :newest_first, lambda { order("pages.created_at DESC")}
+
+  private
+
+  def add_default_permalink
+    if permalink.blank?
+      self.permalink = "#{id}-#{name.parameterize}"
+    end
+  end
+
+  def touch_subject
+    #touch is similiar to:
+    #subject.update_attribute(:update_at , Time.now)
+    subject.touch
+  end
+
+  def delete_related_sections
+    self.secions.each do |section|
+      #or perhaps instead of destroy you would
+      #move tehm to a abother page
+      #section.destroy
+  end
+end
